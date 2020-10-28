e149c751afccc66242660c6f2dce34a2e4bd3ce3
ArthurF23 <71910656+ArthurF23@users.noreply.github.com>
2020-10-28 14:26:00 -0400

    Update main.cpp

--- Yahtzee_GUI/directx9_imgui/main.cpp
+++ Yahtzee_GUI/directx9_imgui/main.cpp
@@ -2,9 +2,6 @@
 // If you are new to Dear ImGui, read documentation from the docs/ folder + read the top of imgui.cpp.
 // Read online: https://github.com/ocornut/imgui/tree/master/docs
 #pragma disable(warning: 4996)
-
-//#include <fstream>
-//#include <cstdint>
 #include <random>
 #include <chrono>
 #include <string>
